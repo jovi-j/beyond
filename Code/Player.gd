f75b078021d99e9c3a76a1f4c0041fac87cea9de
jovi-j <joao.oliveira.121@ufrn.edu.br>
2023-05-24 22:25:11 -0300

    feat:more improvements on attack

--- Code/Player.gd
+++ Code/Player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 
 @onready var sprite : Sprite2D = $SadieSprite
 @onready var anim_player : AnimationPlayer = $SadieAnimPlayer
+@onready var weapon_hitbox : CollisionShape2D = $SadieSprite/WeaponHitbox/WeaponCollision
 
 func _ready():
 	up_direction = Vector2.UP
@@ -14,5 +15,7 @@ func _ready():
 func _physics_process(_delta):
 	if velocity.x < 0:
 		sprite.flip_h = true
+		weapon_hitbox.position.x = -70
 	elif velocity.x > 0:
 		sprite.flip_h = false
+		weapon_hitbox.position.x = 70
--- Scenes/Characters/Attack.gd
+++ Scenes/Characters/Attack.gd
@@ -6,7 +6,11 @@ func enter(_msg := {}) -> void:
 
 func _on_sadie_anim_player_animation_finished(anim_name):
 	if(anim_name == "attack"):
-		if player.velocity.is_zero_approx() and player.is_on_floor():
+		if player.velocity.x == 0 and player.is_on_floor():
 			state_machine.transition_to("Idle")
+		elif player.velocity.x != 0 and player.is_on_floor():
+			state_machine.transition_to("Run")
 		elif not player.is_on_floor():
 			state_machine.transition_to("Air")
+	else:
+		state_machine.transition_to("Idle")
